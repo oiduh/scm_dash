eabf51fb02d80efb44ab7c5fe1326a618ddd2a65
oiduh <m.bialek.95@gmail.com>
2023-09-15 07:51:43 +0200

    proper node removing; start adding edges

--- callback_test.py
+++ callback_test.py
@@ -1,13 +1,14 @@
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Set
 from dash import ALL, MATCH, callback, Input, Output, Dash, html, State, ctx
 from dash.dcc import Dropdown
 import dash_bootstrap_components as dbc
+from copy import deepcopy
 
 
 
-GRAPH = {
-    'a': ['b'],
-    'b': [],
+GRAPH: Dict[str, Set[str]] = {
+    'a': set('b'),
+    'b': set(),
 }
 
 INDEX = {
@@ -38,16 +39,22 @@ class GraphBuilder:
 
     def can_add_edge(self, new_edge: Tuple[str, str]):
         cause, effect = new_edge
-        graph_copy = self.graph.copy()
+        graph_copy = deepcopy(self.graph)
+
+        if cause in self.graph and effect in self.graph[cause]:
+            return False, graph_copy
+
         if (g:=graph_copy.get(cause, None)) is not None:
-            g.append(effect)
+            assert isinstance(g, set), "error"
+            g.add(effect)
         else:
-            graph_copy.update({cause: [effect]})
+            graph_copy.update({cause: set(effect)})
 
+        # if adding node alwasy succeeds, this might be redundant
         if (g:=graph_copy.get(effect, None)) is None:
-            graph_copy.update({effect: []})
+            graph_copy.update({effect: set()})
 
-        return GraphBuilder.is_cyclic(graph_copy), graph_copy
+        return not GraphBuilder.is_cyclic(graph_copy), graph_copy
 
     @staticmethod
     def is_cyclic_util(node, visited, rec_stack, graph):
@@ -80,30 +87,62 @@ class GraphBuilder:
             last = cpy[-1]
             new = cpy[:-1] + chr(ord(last) + 1)
             GraphBuilder.current_id = new
-        self.graph[GraphBuilder.current_id] = []
+        self.graph[GraphBuilder.current_id] = set()
         self.index[index] = GraphBuilder.current_id
+
+    def remove_node(self, node_to_remove):
+        assert node_to_remove in self.graph, "error"
+        assert node_to_remove in self.index.values(), "error"
+        self.graph.pop(node_to_remove, None)
+        for _, effects in self.graph.items():
+            if node_to_remove in effects:
+                effects.discard(node_to_remove)
+        for index, node in self.index.items():
+            if node_to_remove == node:
+                self.index.pop(index, None)
+                break
+
             
 
 class NodeComponent(html.Div):
-    def __init__(self, cause, effects):
+    def __init__(self, cause: str, effects: Set[str]):
+        global graph_builder
         super().__init__(id="", children=None)
+        self.graph_builder = graph_builder
         self.id = {"type": "node", "index": cause} 
         self.style = {
             'border': '2px black solid',
             'margin': '2px'
         }
         self.label = cause
+
+        potential_effects = []
+        for node in self.graph_builder.graph.keys():
+            edge = cause, node
+            can_add_edge = graph_builder.can_add_edge(edge)[0]
+            if can_add_edge:
+                potential_effects.append(node)
+
         self.children = [
             html.Div([
-                html.P(f"variable: {cause}"),
-                html.P(f"effects: {', '.join(effects) if effects else 'None'}"),
-                html.P(id={"type": "textbox", "index": cause}),
-                html.Button("-node", id={"type": "rem-node", "index": cause},
-                            n_clicks=0),
-                html.Button("+edge", id={"type": "add-edge", "index": cause},
-                            n_clicks=0),
-                html.Button("-edge", id={"type": "rem-edge", "index": cause},
-                            n_clicks=0),
+                dbc.Row([
+                    dbc.Col([
+                        html.P(f"variable: {cause}"),
+                        html.P(f"effects: {', '.join(effects) if effects else 'None'}"),
+                        html.P(id={"type": "textbox", "index": cause}),
+                        html.Button("-node", id={"type": "rem-node", "index": cause},
+                                    n_clicks=0),
+                    ]),
+                    dbc.Col([
+                        Dropdown(options=potential_effects, value=None, id={"type": "valid-edges", "index": cause}),
+                        html.Button("+edge", id={"type": "add-edge", "index": cause},
+                                    n_clicks=0),
+                    ]),
+                    dbc.Col([
+                        html.Button("-edge", id={"type": "rem-edge", "index": cause},
+                                    n_clicks=0),
+                    ])
+                ]),
             ], style={'margin': '4px'})
         ]
 
@@ -121,14 +160,16 @@ class GraphBuilderComponent(html.Div):
             'border': '2px black solid',
             'margin': '2px'
         }
+        print(self.graph_builder.graph)
         for cause, effects in self.graph_builder.graph.items():
             new_node = NodeComponent(cause, effects)
+            print(new_node.graph_builder.graph)
             self.children.append(new_node)
 
     def add_node(self, index):
         self.graph_builder.new_node(index)
         var_name = list(self.graph_builder.graph.keys())[-1]
-        new_node = NodeComponent(var_name, [])
+        new_node = NodeComponent(var_name, set())
         self.children.append(new_node)
 
     def add_edge(self):
@@ -137,13 +178,10 @@ class GraphBuilderComponent(html.Div):
     def remove_node(self, node_to_remove):
         for idx, node in enumerate(self.children):
             assert isinstance(node, NodeComponent), "error"
-            print(f"{node.label} - {node_to_remove}")
             if node.label == node_to_remove:
-                print(f"removing: {node.label}")
+                self.graph_builder.remove_node(node_to_remove)
                 self.children[idx:] = self.children[(idx+1):]
                 break
-
-
 
     def remove_edge(self):
         pass
@@ -157,7 +195,7 @@ graph_builder_component = GraphBuilderComponent(id='graph-builder-component')
 # HOW IT SHOULD BE USED IN A MAIN FILE #
 #                                      #
 ########################################
-app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 app.layout = html.Div([
     html.Div("graph builder"),
     html.Hr(),
@@ -192,34 +230,23 @@ app.layout = html.Div([
 @callback(
     Output('graph-builder-component', 'children', allow_duplicate=True),
     Input('add-node-button', 'n_clicks'),
-    State('graph-builder-component', 'children'),
     prevent_initial_call=True
 )
-def add_new_node(index, current_state):
+def add_new_node(index):
     global graph_builder_component
     graph_builder_component.add_node(index)
     return graph_builder_component.children
 
 @callback(
-    # Output({"type": "textbox", "index": MATCH}, "children"),
     Output("graph-builder-component", "children", allow_duplicate=True),
     Input({"type": "rem-node", "index": ALL}, "n_clicks"),
     prevent_initial_call=True
 )
 def remove_node(x):
     global graph_builder_component
-
-    if sum(x) == 0:
-        return graph_builder_component.children
-
     triggered_node = ctx.triggered_id
-    if triggered_node and (node:=triggered_node.get("index", None)):
-        print(f"remove: {node}")
+    if sum(x) > 0 and triggered_node and (node:=triggered_node.get("index", None)):
         graph_builder_component.remove_node(node)
-
-
-    # node = state.get("index", "ERROR")
-    # graph_builder_component.remove_node(node_to_remove)
     return graph_builder_component.children
 
 

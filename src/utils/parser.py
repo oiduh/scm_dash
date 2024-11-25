bf576e11de945beffac432399bb9e5dd6fe1e8f2
oiduh <m.bialek.95@gmail.com>
2024-11-25 08:51:06 +0000

    lexer, parser with ply

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -1,133 +1,166 @@
-from enum import StrEnum
-import string
+import ply.lex as lex
+import ply.yacc as yacc
+import os
 
 
-"""
-very simple parser for mathematical equations for the mechanism
-parser only used for verifaction of input and extration of variables and 'builtin' functions
-"""
+class Parser:
+    """
+    Base class for a lexer/parser that has the rules defined as methods
+    """
+    tokens = ()
+    precedence = ()
 
-class Symbols(StrEnum):
-    LPAREN = "LPAREN"
-    RPAREN = "RPAREN"
-    PLUS = "PLUS"
-    MINUS = "MINUS"
-    MULTIPLY = "MULTIPLY"
-    DIVIDE = "DIVIDE"
-    MODULO = "MODULO"
-    EXPONENT = "EXPONENT"
-    FLOOR_DIVIDE = "FLOOR_DIVIDE"
-    LESS_EQUAL = "LESS_EQUAL"
-    GREATER_EQUAL = "GREATER_EQUAL"
-    LESS = "LESS"
-    GREATER = "GREATER"
-    EQUAL = "EQUAL"
-    NOT_EQUAL = "NOT_EQUAL"
-    AND = "AND"
-    OR = "OR"
-    XOR = "XOR"
-    NOT = "NOT"
+    def __init__(self, **kw):
+        self.debug = kw.get('debug', 0)
+        self.names = {}
+        try:
+            modname = os.path.split(os.path.splitext(__file__)[0])[
+                1] + "_" + self.__class__.__name__
+        except:
+            modname = "parser" + "_" + self.__class__.__name__
+        self.debugfile = modname + ".dbg"
+        # print self.debugfile
 
+        # Build the lexer and parser
+        lex.lex(module=self, debug=self.debug)
+        yacc.yacc(module=self,
+                  debug=self.debug,
+                  debugfile=self.debugfile)
 
-SYMBOL_MAP = {
-    "(" : Symbols.LPAREN,
-    ")" : Symbols.RPAREN,
-    "+" : Symbols.PLUS,
-    "-" : Symbols.MINUS,
-    "*" : Symbols.MULTIPLY,
-    "**" : Symbols.EXPONENT,
-    "/" : Symbols.DIVIDE,
-    "//" : Symbols.FLOOR_DIVIDE,
-    "%" : Symbols.MODULO,
-    "<=" : Symbols.LESS_EQUAL,
-    ">=" : Symbols.GREATER_EQUAL,
-    "<" : Symbols.LESS,
-    ">" : Symbols.GREATER,
-    "=" : Symbols.EQUAL,
-    "!=" : Symbols.NOT_EQUAL,
-    "&" : Symbols.AND,
-    "|" : Symbols.OR,
-    "^" : Symbols.XOR,
-    "!" : Symbols.NOT,
-}
-
-
-class TokenType(StrEnum):
-    SYMBOL = "SYMBOL"
-    ID = "ID"
-    NUMBER = "NUMBER"
-    INVALID_SYMBOL = "INVALID_SYMBOL"
-    INVALID_NUMBER = "INVALID_NUMBER"
-
-
-class Token:
-    def __init__(self, token_type: TokenType, value: str) -> None:
-        self.id = token_type
-        self.value = value
-
-    def __str__(self) -> str:
-        return f"({self.id}, {self.value})"
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-
-def tokenize(input: str) -> list[Token]:
-
-    tokens: list[Token] = []
-    idx = 0
-    while idx < len(input):
-        char = input[idx]
-        if char in string.whitespace:
-            idx += 1
-            continue
-        if idx < len(input) - 1 and (m:=input[idx:idx+2]) in SYMBOL_MAP:
-            # print(TOKEN_MAP[m], end="")
-            idx += 2
-            tokens.append(Token(TokenType.SYMBOL, SYMBOL_MAP[m]))
-        elif char in SYMBOL_MAP:
-            # print(TOKEN_MAP[char], end="")
-            idx += 1
-            tokens.append(Token(TokenType.SYMBOL, SYMBOL_MAP[char]))
-        elif char.isdigit() or char == ".":
-            num_str = [char]
-            skip = idx + 1
-            while skip < len(input) and (next:=input[skip]) and (next.isdigit() or next == "."):
-                num_str.append(next)
-                skip += 1
-            idx = skip
-            num_str = "".join(num_str)
+    def run(self):
+        while True:
             try:
-                num = float(num_str)
-                # print(f"<NUM: '{num}'>", end="")
-                tokens.append(Token(TokenType.NUMBER, str(num)))
-            except Exception:
-                # print("<INVALID_NUMBER>", end="")
-                tokens.append(Token(TokenType.INVALID_NUMBER, num_str))
-        elif char in string.ascii_letters or char == "_":
-            name = [char]
-            skip = idx + 1
-            while skip < len(input) and (next:=input[skip]) and (next in string.ascii_letters or next == "_"):
-                name.append(next)
-                skip += 1
-            idx = skip
-            name = "".join(name)
-            # print(f"<VAR: '{name}'>", end="")
-            tokens.append(Token(TokenType.ID, name))
+                s = input('calc > ')
+            except EOFError:
+                break
+            if not s:
+                continue
+            yacc.parse(s)
+
+    def run_example(self, example: str):
+        yacc.parse(example)
+
+
+class Calc(Parser):
+
+    tokens = (
+        'NAME', 'NUMBER',
+        'PLUS', 'MINUS', 'EXP', 'TIMES', 'DIVIDE', 'EQUALS', 'NOT_EQUALS', 'INT_DIV',
+        'LESS', 'GREATER', 'LESS_EQUAL', 'GREATER_EQUAL', 'NOT', 'AND', 'OR', 'XOR',
+        'LPAREN', 'RPAREN',
+    )
+
+    # Tokens
+
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_EXP = r'\*\*'
+    t_TIMES = r'\*'
+    t_INT_DIV = r'//'
+    t_DIVIDE = r'/'
+    t_EQUALS = r'='
+    t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t_NOTEQUALS = r'!='
+    t_NOT = r'~'
+    t_LESS_EQUAL = r'<='
+    t_GREATER_EQUAL = r'>='
+    t_LESS = r'<'
+    t_GREATER = r'>'
+    t_AND = r'&'
+    t_OR = r'|'
+    t_XOR = r'^'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+
+    def t_NUMBER(self, t):
+        r'\d+'
+        try:
+            t.value = int(t.value)
+        except ValueError:
+            print("Integer value too large %s" % t.value)
+            t.value = 0
+        # print "parsed number %s" % repr(t.value)
+        return t
+
+    t_ignore = " \t"
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count("\n")
+
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    # Parsing rules
+
+    precedence = (
+        ('left', 'PLUS', 'MINUS'),
+        ('left', 'TIMES', 'DIVIDE'),
+        ('left', 'EXP'),
+        ('right', 'UMINUS', 'UNOT'),
+    )
+
+    def p_statement_assign(self, p):
+        'statement : NAME EQUALS expression'
+        self.names[p[1]] = p[3]
+
+    def p_statement_expr(self, p):
+        'statement : expression'
+        print(p[1])
+
+    def p_expression_binop(self, p):
+        """
+        expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression
+                  | expression EXP expression
+        """
+        # print [repr(p[i]) for i in range(0,4)]
+        if p[2] == '+':
+            p[0] = p[1] + p[3]
+        elif p[2] == '-':
+            p[0] = p[1] - p[3]
+        elif p[2] == '*':
+            p[0] = p[1] * p[3]
+        elif p[2] == '/':
+            p[0] = p[1] / p[3]
+        elif p[2] == '**':
+            p[0] = p[1] ** p[3]
+
+    def p_expression_uminus(self, p):
+        'expression : MINUS expression %prec UMINUS'
+        p[0] = -p[2]
+
+    def p_expression_unot(self, p):
+        'expression : NOT expression %prec UNOT'
+        p[0] = ~p[2]
+
+    def p_expression_group(self, p):
+        'expression : LPAREN expression RPAREN'
+        p[0] = p[2]
+
+    def p_expression_number(self, p):
+        'expression : NUMBER'
+        p[0] = p[1]
+
+    def p_expression_name(self, p):
+        'expression : NAME'
+        try:
+            p[0] = self.names[p[1]]
+        except LookupError:
+            print("Undefined name '%s'" % p[1])
+            p[0] = 0
+
+    def p_error(self, p):
+        if p:
+            print("Syntax error at '%s'" % p.value)
         else:
-            # print(f"<INVALID_CHAR: '{char}'>", end="")
-            idx += 1
-            tokens.append(Token(TokenType.INVALID_SYMBOL, char))
-
-    return tokens
+            print("Syntax error at EOF")
 
 
-def parse(tokens: list[Token]) -> bool:
-    return False
-
-
-if __name__ == "__main__":
-    x = tokenize("0.0 + a + sin(a b) !")
-    y = parse(x)
-    print(x)
-
+if __name__ == '__main__':
+    calc = Calc()
+    # calc.run()
+    calc.run_example("(1 + 2) * a")

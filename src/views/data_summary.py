2ba859fb966705c49159ba5920ef7805f73bdd03
oiduh <m.bialek.95@gmail.com>
2025-04-23 08:36:53 +0200

    raw stats latexify

--- src/views/data_summary.py
+++ src/views/data_summary.py
@@ -196,12 +196,14 @@ class StaticGraph(html.Div):
             data_graph = ff.create_distplot(
                 [data], [node.name or node.id_], show_rug=False, bin_size=0.2, colors=["green"]
             )
+            legend = False
         else:
             unique, counts = np.unique(data, return_counts=True)
             data_graph = go.Figure(go.Pie(values=counts, labels=[str(x) for x in unique]))
+            legend = True
         data_graph.update_layout(
             {
-                "showlegend": False,
+                "showlegend": legend,
                 "height": 300,
                 "width": 700,
                 "margin_l": 0,
@@ -317,14 +319,16 @@ class RawStatsViewer(html.Div):
             assert row.children is not None
             for jdx in range(3):
                 kdx = idx + jdx
-                print(f"{idx=}, {kdx=}")
                 if kdx >= num_nodes:
                     break
                 node = nodes[kdx]
                 cur_col = row.children[jdx]
                 cur_col.children = []
+
+                # TODO:find a nice way to display the range of the data
+
                 cur_col.children.append(html.H5(f"Node: {node.id_}"))
-                in_nodes = "\\{\\}" if len(node.in_nodes) > 0 else "\\{" + ", ".join(node.in_nodes) + "\\}"
+                in_nodes = "\\{\\}" if len(node.in_nodes) > 1 else "\\{" + ", ".join(node.in_nodes) + "\\}"
                 cur_col.children.append(dcc.Markdown(f"$${node.id_}_{{in}} = {in_nodes}$$", mathjax=True))
                 out_nodes = "\\{\\}" if len(node.out_nodes) < 1 else "\\{" + ", ".join(node.out_nodes) + "\\}"
                 cur_col.children.append(dcc.Markdown(f"$${node.id_}_{{out}} = {out_nodes}$$", mathjax=True))
@@ -332,20 +336,27 @@ class RawStatsViewer(html.Div):
                 cur_col.children.append(dcc.Markdown(f"$$n_{{{node.id_}}} := \\{{ x | x \\in [{noise.min():.4f}, {noise.max():.4f}] \\}}$$", mathjax=True))
                 assert node.data is not None
                 cur_col.children.append(dcc.Markdown(f"$$data_{{{node.id_}}} := \\{{ x | x \\in [{node.data.min():.4f}, {node.data.max():.4f}] \\}}$$", mathjax=True))
+
+                causes = node.in_nodes
+                causes.append(f"n_{node.id_}")
+                causes = ", ".join(causes)
                 if node.mechanism_metadata.mechanism_type == "regression":
                     cur_col.children.append(dcc.Markdown("Mechanism type: regression"))
-                    # todo: fix the formulas, as in views/mechanism.py
-                    x = py_to_latex(f"f({causes})", in_nodes)
-                    y = py_to_latex(f"{list(formulas.values())[0]}", in_nodes)
-                    latex_formula = x + ":=" + y
-                    cur_col.children.append(dcc.Markdown(f"{node.mechanism_metadata.formulas['0']}"))
+                    # TODO: fix the formulas, as in views/mechanism.py
+                    x = f"f({causes})"
+                    y = py_to_latex(f"{list(node.mechanism_metadata.formulas.values())[0]}", node.in_nodes)
+                    formula = x + ":=" + y
+                    cur_col.children.append(dcc.Markdown(f"$${formula}$$", mathjax=True))
                 else:
                     cur_col.children.append(dcc.Markdown("Mechanism type: classification"))
-                    for formula in node.mechanism_metadata.get_formulas().values():
-                        cur_col.children.append(dcc.Markdown(f"{formula}"))
+                    for class_id, formula in node.mechanism_metadata.get_formulas().items():
+                        x = f"f_{class_id}({causes})"
+                        y = py_to_latex(f"{list(node.mechanism_metadata.formulas.values())[0]}", node.in_nodes)
+                        formula = x + ":=" + y
+                        cur_col.children.append(dcc.Markdown(f"$${formula}$$", mathjax=True))
+                    else_class = f"f_{{else}}({causes})"
+                    cur_col.children.append(dcc.Markdown(f"$${else_class}$$", mathjax=True))
             self.children.append(row)
-            # dcc.Markdown(f"$$f({', '.join(displayed_names)}):=$$", mathjax=True),
-            # width="auto", style={"paddingTop": "10px"}
 
 
 class NodeViewer(html.Div):
